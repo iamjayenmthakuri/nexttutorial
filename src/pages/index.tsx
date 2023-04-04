37a032c7cd0340f02d44bda26e3f378878d283ba
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-04 11:38:20 +0545

    reating home page and rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -5,7 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <div className="home">Hello</div>
+      <Head>
+        <title>NextTutorial</title>
+      </Head>
+      <div className={styles.homePageTitle}>Hello</div>
+      <Link href="/about">About</Link>
     </>
   );
 }
